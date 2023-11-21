fb9baeabfe1a2a1f7530f191ce840b0122770d89
pepperonipizza97 <potatopizza715@gmail.com>
2023-11-21 13:42:42 +0900

    Update urls.py

--- mml/user/urls.py
+++ mml/user/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('login/', views.login_user, name='login')
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
     # 여기에 더 많은 사용자 관련 URL 패턴을 추가할 수 있습니다.
 ]
