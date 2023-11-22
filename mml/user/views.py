a5ff785c7379ceb314a267a1688b0a7435a3e967
pepperonipizza97 <potatopizza715@gmail.com>
2023-11-22 18:40:33 +0900

    Update views.py

--- mml/user/views.py
+++ mml/user/views.py
@@ -6,7 +6,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import get_user_model
-from .serializers import MMLUserInfoSerializer
+from .serializers import UserSerializer
 from django.contrib.auth import authenticate
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
