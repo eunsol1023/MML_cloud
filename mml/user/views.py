fdf2dac96168abddeb749907944843143cdfb1e7
pepperonipizza97 <potatopizza715@gmail.com>
2023-11-22 18:41:15 +0900

    Update views.py

--- mml/user/views.py
+++ mml/user/views.py
@@ -1,20 +1,40 @@
 # user/views.py
 
 from datetime import datetime
+import logging
 from dateutil.relativedelta import relativedelta
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import get_user_model
-from .serializers import UserSerializer
-from django.contrib.auth import authenticate
-from rest_framework.views import APIView
+from .serializers import MMLUserInfoSerializer
+from django.contrib.auth import authenticate, login
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse, HttpResponseBadRequest
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import AllowAny
+from django.utils import timezone
+from django.contrib.auth import logout
+from django.http import HttpResponse
+from django.middleware.csrf import get_token
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login, logout as auth_logout
+from django.shortcuts import redirect
+from django.shortcuts import render
 from rest_framework_simplejwt.tokens import RefreshToken
-from .serializers import UserSerializer
+
+
+# Create a logger instance
+logger = logging.getLogger(__name__)
+
+User = get_user_model()
 
 @api_view(['POST'])
 def signup(request):
-    
+    """
+    Create a new user instance.
+    """
     data = request.data
 
     # age_range 필드가 None이 아닌 경우에 연령대로 변환
@@ -43,34 +63,26 @@ def signup(request):
     data['age_range'] = age_range
 
     serializer = MMLUserInfoSerializer(data=data)
-    
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class LoginView(APIView):
-    def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        user = authenticate(username=username, password=password)
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def login_user(request):
+    form = AuthenticationForm(request, data=request.data)  # request.POST 대신 request.data 사용
+    if form.is_valid():
+        auth_login(request, form.get_user())
 
-        if user is not None:
-            refresh = RefreshToken.for_user(user)
-            return Response({
-                'refresh': str(refresh),
-                'access': str(refresh.access_token),
-                'user': UserSerializer(user).data
-            }, status=status.HTTP_200_OK)
-        else:
-            return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
-
-class LogoutView(APIView):
-    def post(self, request):
-        try:
-            refresh_token = request.data.get('refresh')
-            token = RefreshToken(refresh_token)
-            token.blacklist()
-            return Response(status=status.HTTP_205_RESET_CONTENT)
-        except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        # JSON 응답 반환
+        return JsonResponse({'message': 'Login successful'}, status=200)
+    else:
+        # 폼 유효성 검증 실패 시, 오류 메시지 반환
+        return JsonResponse({'errors': form.errors}, status=400)
+    
+@api_view(['POST'])
+@csrf_exempt
+def logout_user(request):
+    auth_logout(request)
+    return JsonResponse({'message': 'Logged out'})
