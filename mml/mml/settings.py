a47acebdad8118c65268cf164201dee828d5f3aa
pepperonipizza97 <potatopizza715@gmail.com>
2023-11-23 17:24:38 +0900

    Update settings.py

--- mml/mml/settings.py
+++ mml/mml/settings.py
@@ -60,7 +60,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "yourapp.context_processors.global_user_context",
+                "mml.context_processors.global_user_context",
             ],
         },
     },
