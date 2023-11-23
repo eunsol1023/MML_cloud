d631e95e31c65e8685c59935b5c3ff8626ace492
syooh <104604079+syooh@users.noreply.github.com>
2023-11-23 10:31:00 +0900

    django

--- mml/mml/urls.py
+++ mml/mml/urls.py
@@ -5,5 +5,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('user.urls')), # users 앱의 URL 포함시킵니다.
+    path('user/', include('user.urls')), # user 앱의 URL 포함시킵니다.
+    path('music/', include('music.urls')),
 ]
