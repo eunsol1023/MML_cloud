790e7622016b2f577340309278d7b5dcc88f0490
syooh <104604079+syooh@users.noreply.github.com>
2023-11-24 11:53:11 +0900

    django

--- mml/music/urls.py
+++ mml/music/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import music_reco_view, user_like_artist_view, song2vec_view, tag_song2vec_view
+from .views import user_like_artist_view, song2vec_view, tag_song2vec_view
 
 urlpatterns = [
-    path('music_reco/', music_reco_view.as_view()),
     path('user_like_artist/', user_like_artist_view.as_view()),
     path('song2vec/', song2vec_view.as_view()),
     path('tag_song2vec/', tag_song2vec_view.as_view()),
