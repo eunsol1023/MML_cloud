14f41b80b427f96d40c949e564b9638ad264c17e
pepperonipizza97 <potatopizza715@gmail.com>
2023-11-25 17:46:13 +0900

    Revert "Update views.py"
    This reverts commit bc1bf5bb6b20dcf5b9142fa4e1a9a7054001aa7b.

--- mml/user/views.py
+++ mml/user/views.py
@@ -3,17 +3,26 @@
 from datetime import datetime
 import logging
 from dateutil.relativedelta import relativedelta
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import AllowAny
 from django.contrib.auth import get_user_model
 from .serializers import MMLUserInfoSerializer
+from django.contrib.auth import authenticate, login
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse, HttpResponseBadRequest
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import AllowAny
+from django.utils import timezone
+from django.contrib.auth import logout
+from django.http import HttpResponse
+from django.middleware.csrf import get_token
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as auth_login, logout as auth_logout
-from django.http import JsonResponse
-from django.utils.decorators import method_decorator
-from django.middleware.csrf import get_token
+from django.shortcuts import redirect
+from django.shortcuts import render
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 # Create a logger instance
@@ -59,24 +68,21 @@ def signup(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-logger = logging.getLogger(__name__)
-
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def login_user(request):
-    form = AuthenticationForm(request, data=request.data)
+    form = AuthenticationForm(request, data=request.data)  # request.POST 대신 request.data 사용
     if form.is_valid():
-        user = form.get_user()
-        auth_login(request, user)
-        logger.info(f'Login successful for user: {user.username}')
+        auth_login(request, form.get_user())
+
+        # JSON 응답 반환
         return JsonResponse({'message': 'Login successful'}, status=200)
     else:
-        logger.warning(f'Login failed: {form.errors.as_json()}')
-        return JsonResponse({'errors': form.errors.get_json_data()}, status=401)
-
+        # 폼 유효성 검증 실패 시, 오류 메시지 반환
+        return JsonResponse({'errors': form.errors}, status=400)
+    
 @api_view(['POST'])
+@csrf_exempt
 def logout_user(request):
-    username = request.user.username
     auth_logout(request)
-    logger.info(f'Logout successful for user: {username}')
-    return JsonResponse({'message': 'Logged out'}, status=200)
+    return JsonResponse({'message': 'Logged out'})
