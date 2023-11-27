66a59c0d2dcccadabafeb99c69544451822b2c65
pepperonipizza97 <potatopizza715@gmail.com>
2023-11-27 19:25:13 +0900

    Update settings.py

--- mml/mml/settings.py
+++ mml/mml/settings.py
@@ -126,7 +126,7 @@ CSRF_COOKIE_SECURE = True
 CSRF_COOKIE_HTTPONLY = False
 # CSRF_COOKIE_PATH = '/'
 # CSRF_COOKIE_DOMAIN = '.cloudmml.com'
-# CSRF_TRUSTED_ORIGINS = ['http://*.cloudmml.com']
+CSRF_TRUSTED_ORIGINS = ['http://*.cloudmml.com']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
