50a8e29e04f7e597fe7e9343b459b73a53fb47d9
pepperonipizza97 <potatopizza715@gmail.com>
2023-11-27 12:08:11 +0900

    Update views.py

--- mml/music/views.py
+++ mml/music/views.py
@@ -20,26 +20,20 @@ import logging
 logger = logging.getLogger(__name__)
 
 def song_info(request):
-    # Check if it's a GET request
     if request.method == 'GET':
-        # Get parameters from the request
+        # Django automatically decodes these parameters
         title = request.GET.get('title', None)
         artist = request.GET.get('artist', None)
 
-        # Logging the received parameters for debugging
         logger.info(f"Received title: {title}, artist: {artist}")
 
-        # Validate the parameters
         if not title or not artist:
             return JsonResponse({'error': 'Missing required parameters'}, status=400)
 
         try:
-            # Query the MMLMusicInfo model
             song_info = MMLMusicInfo.objects.filter(title=title, artist=artist).first()
 
-            # Check if song exists
             if song_info:
-                # Prepare the response data
                 data = {
                     'title': song_info.title,
                     'artist': song_info.artist,
@@ -48,14 +42,11 @@ def song_info(request):
                 }
                 return JsonResponse(data, status=200)
             else:
-                # Song not found
                 return JsonResponse({'error': 'Song not found'}, status=404)
 
         except Exception as e:
-            # Handle any other exceptions
             logger.error(f"An error occurred: {str(e)}")
             return JsonResponse({'error': str(e)}, status=500)
 
     else:
-        # If not a GET request, return 405 Method Not Allowed
         return JsonResponse({'error': 'Method not allowed'}, status=405)
