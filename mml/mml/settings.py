c8ba77f6f4ef8692c197498f422f1552b10c9578
pepperonipizza97 <potatopizza715@gmail.com>
2023-11-27 19:07:42 +0900

    Update settings.py

--- mml/mml/settings.py
+++ mml/mml/settings.py
@@ -122,6 +122,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://api.cloudmml.com" # 도메인 서버 (백)
 ]
 CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = False
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -148,19 +151,4 @@ SESSION_COOKIE_AGE = 180		# 세션쿠키의 유효기간 설정(default: 2주)
 SESSION_SAVE_EVERY_REQUEST = True	# 서버에게 Request를 보낼 경우 시간 초기화
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 SESSION_COOKIE_PATH = '/'
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        '': {
-            'handlers': ['console'],
-            'level': 'INFO',
-        },
-    },
-}
+SESSION_COOKIE_SAMESITE = 'None'
