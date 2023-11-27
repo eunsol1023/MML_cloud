ec3e90f9442efa7c5db72b0ba68fa305e2e627a1
pepperonipizza97 <potatopizza715@gmail.com>
2023-11-27 10:04:18 +0900

    Update song2vec.py

--- mml/music/song2vec.py
+++ mml/music/song2vec.py
@@ -5,7 +5,6 @@ from rest_framework import status
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
-# 기타 필요한 import 문
 from sqlalchemy import create_engine
 from song2vec_data_loader import song2vec_DataLoader
 from sklearn.metrics.pairwise import cosine_similarity
@@ -53,23 +52,42 @@ class song2vec_view(APIView):
         # 모델 로드
         w2v_model = apps.get_app_config('music').model
 
-        def create_lyrics_profile(lyrics_list, w2v_model):
+        def get_top_words_weights(lyrics_list, top_n=20):
+            # 모든 가사를 하나의 리스트로 결합합니다.
+            all_words = [word for lyrics in lyrics_list for word in lyrics]
+            # 가장 흔한 단어와 그 빈도수를 계산합니다.
+            top_words = pd.Series(all_words).value_counts().head(top_n)
+            # 중복을 제거한 단어 리스트를 생성합니다.
+            unique_words = set(top_words.index)
+            # 가중치를 계산합니다: 여기서는 단순화를 위해 빈도수를 그대로 사용하지만,
+            # 다른 가중치 할당 방식을 사용할 수도 있습니다.
+            weights = {word: freq / top_words.max() for word, freq in top_words.items() if word in unique_words}
+            return weights
+        
+        # 사용자의 가사 프로필을 만들 때, 가장 흔한 단어에 가중치를 주어 벡터를 계산하는 함수를 수정합니다.
+        def create_weighted_lyrics_profile(lyrics_list, w2v_model, top_words_weights):
             lyrics_vectors = []
             for lyrics in lyrics_list:
-                lyrics_vector = []
+                # lyrics 벡터의 평균을 계산하기 전에 각 단어에 대한 가중치를 고려합니다.
+                weighted_vectors = []
                 for word in lyrics:
-                    if word in w2v_model.wv:  # 모델의 단어장에 있는 경우에만 처리합니다.
-                        lyrics_vector.append(w2v_model.wv[word])
-                if lyrics_vector:
-                    lyrics_vectors.append(np.mean(lyrics_vector, axis=0))
+                    if word in w2v_model.wv:
+                        weight = top_words_weights.get(word, 1)  # 여기서 get 메소드를 사용
+                        weighted_vectors.append(w2v_model.wv[word] * weight)
+                if weighted_vectors:  # 가중치가 적용된 벡터의 평균을 계산합니다.
+                    lyrics_vectors.append(np.mean(weighted_vectors, axis=0))
             return np.mean(lyrics_vectors, axis=0) if lyrics_vectors else np.zeros(w2v_model.vector_size)
+
+        print('1')
 
         # 사용자별 프로필 벡터를 생성합니다.
         # user_id = 'QrDM6lLc'
         user_lyrics = music_data[music_data['user'] == user_id]['processed_lyrics']
-        test_weights = get_top_words_weights(user_lyrics, test_weight=10)
-        user_profile_vector = create_lyrics_profile(user_lyrics, w2v_model, test_weights)
-        print('c')
+        # 가사 리스트를 사용하여 가중치 사전을 생성
+        weights_dict = get_top_words_weights(user_lyrics)
+        # 사용자의 가사 프로필을 생성
+        user_profile_vector = create_weighted_lyrics_profile(user_lyrics, w2v_model, weights_dict)
+        # user_profile_vector = create_lyrics_profile(user_lyrics, w2v_model)
 
         # 특정 사용자 ID에 대한 사용자의 청취 기록을 필터링'02FoMC0v'
         user_specific_log = music_data[music_data['user'] == user_id]
@@ -83,48 +101,52 @@ class song2vec_view(APIView):
         # 사용자 상위 장르와 일치하는 노래에 대해 music_total_with_genre 데이터 프레임 필터링
         user_specific_top_genres_songs_df = music_tag_data[music_tag_data['genre'].isin(user_specific_top_genres)]
 
-        # # 태그 데이터를 전처리하는 함수를 정의합니다.
-        # def preprocess_tags(tag_string):
-        #     # '#' 기호를 기준으로 태그를 분리합니다.
-        #     tags = tag_string.strip().split('#')
-        #     # 빈 문자열을 제거합니다.
-        #     tags = [tag for tag in tags if tag]  # 공백 태그 제거
-        #     return tags
-        print('d')
+        print('2')
+
+        # 태그 데이터를 전처리하는 함수를 정의합니다.
+        def preprocess_tags(tag_string):
+            # '#' 기호를 기준으로 태그를 분리합니다.
+            tags = tag_string.strip().split('#')
+            # 빈 문자열을 제거합니다.
+            tags = [tag for tag in tags if tag]  # 공백 태그 제거
+            return tags
 
         # 태그 데이터에 전처리 함수를 적용합니다.
         user_specific_top_genres_songs_df['processed_tags'] = user_specific_top_genres_songs_df['tag'].apply(preprocess_tags)
 
-        # # 태그를 벡터로 변환하는 함수를 정의합니다.
-        # def vectorize_tags(tags, w2v_model):
-        #     tag_vectors = []
-        #     for tag in tags:
-        #         # 태그 내의 각 단어에 대해 벡터를 얻고 평균을 계산합니다.
-        #         tag_word_vectors = [w2v_model.wv[word] for word in tag.split() if word in w2v_model.wv]
-        #         if tag_word_vectors:  # 태그가 모델 단어장에 있는 경우에만 평균 벡터를 계산합니다.
-        #             tag_vectors.append(np.mean(tag_word_vectors, axis=0))
-        #     return np.mean(tag_vectors, axis=0) if tag_vectors else np.zeros(w2v_model.vector_size)
+        # 태그를 벡터로 변환하는 함수를 정의합니다.
+        def vectorize_tags(tags, w2v_model):
+            tag_vectors = []
+            for tag in tags:
+                # 태그 내의 각 단어에 대해 벡터를 얻고 평균을 계산합니다.
+                tag_word_vectors = [w2v_model.wv[word] for word in tag.split() if word in w2v_model.wv]
+                if tag_word_vectors:  # 태그가 모델 단어장에 있는 경우에만 평균 벡터를 계산합니다.
+                    tag_vectors.append(np.mean(tag_word_vectors, axis=0))
+            return np.mean(tag_vectors, axis=0) if tag_vectors else np.zeros(w2v_model.vector_size)
 
         # 각 태그를 벡터로 변환합니다.
         user_specific_top_genres_songs_df['tag_vector'] = user_specific_top_genres_songs_df['processed_tags'].apply(lambda tags: vectorize_tags(tags, w2v_model))
 
-        # # 사용자 프로필 벡터와 모든 태그 벡터 사이의 코사인 유사도를 계산하고 상위 N개의 추천과 함께 유사도를 반환하는 함수
-        # def recommend_songs_with_similarity(user_profile_vector, tag_vectors, songs_data, top_n=20):
-        #     # 사용자 프로필 벡터를 코사인 유사도 계산을 위해 reshape
-        #     user_vector_reshaped = user_profile_vector.reshape(1, -1)
+        print('3')
 
-        #     # 모든 태그 벡터와의 유사도 계산
-        #     similarity_scores = cosine_similarity(user_vector_reshaped, tag_vectors)[0]
+        # 사용자 프로필 벡터와 모든 태그 벡터 사이의 코사인 유사도를 계산하고 상위 N개의 추천과 함께 유사도를 반환하는 함수
+        def recommend_songs_with_similarity(user_profile_vector, tag_vectors, songs_data, top_n=20):
+            # 사용자 프로필 벡터를 코사인 유사도 계산을 위해 reshape
+            user_vector_reshaped = user_profile_vector.reshape(1, -1)
 
-        #     # 유사도 점수를 기반으로 상위 N개의 인덱스를 가져옵니다
-        #     top_indices = similarity_scores.argsort()[-top_n:][::-1]
+            # 모든 태그 벡터와의 유사도 계산
+            similarity_scores = cosine_similarity(user_vector_reshaped, tag_vectors)[0]
 
-        #     # 상위 N개의 노래 추천 정보와 유사도 점수를 함께 반환
-        #     recommendations_with_scores = songs_data.iloc[top_indices]
-        #     recommendations_with_scores['similarity'] = similarity_scores[top_indices]
-        #     return recommendations_with_scores[['title', 'artist', 'tag', 'similarity']]
-        print('e')
+            # 유사도 점수를 기반으로 상위 N개의 인덱스를 가져옵니다
+            top_indices = similarity_scores.argsort()[-top_n:][::-1]
 
+            # 상위 N개의 노래 추천 정보와 유사도 점수를 함께 반환
+            recommendations_with_scores = songs_data.iloc[top_indices]
+            recommendations_with_scores['similarity'] = similarity_scores[top_indices]
+            return recommendations_with_scores[['title', 'artist', 'tag', 'similarity']]
+
+        print('4')
+        
         # 모든 태그 벡터를 하나의 배열로 추출합니다.
         tag_vectors_matrix = np.array(list(user_specific_top_genres_songs_df['tag_vector']))
 
@@ -139,7 +161,6 @@ class song2vec_view(APIView):
             on=['title', 'artist'],
             how='inner'
         )
-        print('f')
 
         song2vec_final = song2vec_final[['title', 'artist', 'album_image_url']]
 
