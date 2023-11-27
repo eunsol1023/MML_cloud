a7f232265c8d048f32feefc3815d656b4174fd5a
pepperonipizza97 <potatopizza715@gmail.com>
2023-11-27 10:39:47 +0900

    music

--- mml/music/views.py
+++ mml/music/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import JsonResponse
-from .models import MMLMusicInfo
+from user.models import MMLMusicInfo
 
 from .serializers import *
 
