70fe8cc57b11634a8e239fdd3b4bc8d4393155f8
pepperonipizza97 <potatopizza715@gmail.com>
2023-11-28 15:28:41 +0900

    Update settings.py

--- mml/mml/settings.py
+++ mml/mml/settings.py
@@ -152,3 +152,5 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.db'  # Default
 SESSION_COOKIE_AGE = 600		# 세션쿠키의 유효기간 설정(default: 2주)
 SESSION_SAVE_EVERY_REQUEST = True	# 서버에게 Request를 보낼 경우 시간 초기화
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+DEBUG = True
