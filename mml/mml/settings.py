5f6d68e49b6f7d6be5b56ea22b9974d40097b616
pepperonipizza97 <potatopizza715@gmail.com>
2023-11-28 15:55:34 +0900

    Update settings.py

--- mml/mml/settings.py
+++ mml/mml/settings.py
@@ -138,7 +138,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSE' :[
+    'DEFAULT_PERMISSION_CLASSES' :[
         'rest_framework.permissions.DjangoModelPermissionOrAnonReadOnly'
     ]
 }
