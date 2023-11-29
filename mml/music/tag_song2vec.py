cb64a3b7e092c79185f985edfdac6b70a081d374
pepperonipizza97 <potatopizza715@gmail.com>
2023-11-29 15:06:23 +0900

    Update tag_song2vec.py

--- mml/music/tag_song2vec.py
+++ mml/music/tag_song2vec.py
@@ -1,16 +1,31 @@
+from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+
 from .serializers import *
+
 import pandas as pd
 import numpy as np
+
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+
+from konlpy.tag import Okt
+
+import Levenshtein as lev
+
+from scipy.stats import pearsonr
+
 from sqlalchemy import create_engine
+
 from django.apps import apps
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+
 from song2vec_data_loader import song2vec_DataLoader
+
 from django.contrib.sessions.models import Session
 from user.models import MMLUserInfo
 
@@ -21,12 +36,10 @@ song2vec_data_loader = song2vec_DataLoader(engine)
 
 mml_user_his_df, mml_music_info_df, mml_music_tag_df, music_data, music_tag_data = song2vec_data_loader.song2vec_load_data()
 
-# user_id = 'zmffkdnem'
-
 class tag_song2vec_view(APIView):
     def get(self, request):
         print('==========4번==========')
-
+        
         session_key = request.COOKIES.get("sessionid")
     
         if session_key:
@@ -48,97 +61,61 @@ class tag_song2vec_view(APIView):
                 print("Session with key does not exist")
         else:
             print("Session key does not exist")
+
         # 모델 로드
         w2v_model = apps.get_app_config('music').model
 
-        # 가사 데이터 불러오기
         processed_lyrics = pd.read_csv('./music/files/processed_lyrics.csv')
 
-        # 'Title' 및 'Artist'를 사용하여 데이터프레임 병합하여 일치하는 노래 찾기
+        # Merge the dataframes on 'Title' and 'Artist' to find matching songs
         matched_songs_df = pd.merge(
             mml_music_info_df, mml_user_his_df,
             on=['title', 'artist'],
             how='inner',
             suffixes=('_all_music', '_user_log')
         )
-        
-        # 필요한 음악 데이터 추출
+
         music_data = matched_songs_df[['user', 'title', 'artist', 'genre_user_log', 'playtime', 'created_at', 'lyrics']]
 
-        # 가사 데이터와 병합
         music_data = music_data.join(processed_lyrics)
 
-        # 사용자로부터의 입력 문장
-        input_sentence = request.GET.get('input_sentence', None)
-        print("input_sentence의 값 : ", input_sentence)
-        print("input_sentence의 타입:", type(input_sentence))
-        if not input_sentence:
-            return Response({'error': 'input_sentence가 필요합니다.'}, status=status.HTTP_400_BAD_REQUEST)
+        # Okt 객체 생성
+        okt = Okt()
 
-        # 문장을 토큰화
-        tokens = word_tokenize(input_sentence)
+        # 불용어 목록 정의
+        stopwords = set(['의', '가', '이', '은', '들', '는', '좀', '잘', '걍', '과', '도', '를', '으로', '자', '에', '와', '한', '하다'])
 
-        # 불용어 제거 (키워드 추출에 유용하지 않은 일반적인 단어)
-        # 참고: 한국어의 경우 nltk의 기본은 영어용이므로 한국어에 맞는 불용어 리스트가 필요할 수 있습니다.
-        filtered_tokens = [word for word in tokens if word not in stopwords.words('english')]
+        def find_similar_tags(word, tag_list, threshold=0.5):
+            # 주어진 단어와 태그 목록 사이의 유사한 태그를 찾는 함수
+            similar_tags = []
+            for tag in tag_list:
+                similarity = lev.ratio(word, tag)
+                if similarity >= threshold:
+                    similar_tags.append((tag, similarity))
+            return similar_tags
 
-        # 고유한 'Title' 열의 값을 확인하여 형식을 이해하기
-        unique_titles = mml_music_tag_df['title'].unique()
+        # 문장에서 형태소 추출 및 불용어 제거
+        sentence = '봄 아침 산책'
+        morphs = [morph for morph in okt.morphs(sentence) if morph not in stopwords]
 
-        # 'Tag' 열 처리: 태그를 개별 항목으로 분할하여 새로운 데이터프레임 생성
-        tag_data_expanded = mml_music_tag_df.drop('tag', axis=1).join(
-            mml_music_tag_df['tag'].str.split('#', expand=True).stack().reset_index(level=1, drop=True).rename('tag')
-        )
+        # 태그 목록
+        tags = [
+            "감성적인", "팝송", "밤", "새벽", "카페", "휴식", "명상", "드라이브", "신나는", "가을", "인디", "그루브한", "발라드한", "기분전환", "경쾌한", "여름", "겨울", "힐링", "잔잔한", "연주곡", "재즈", "산책", "여행", "R&B", "사랑", "기쁨", "스트레스", "짜증", "공연", "페스티벌", "봄", "그리움", "일상", "저녁", "이별", "슬픔", "화창한", "힙합", "락", "비", "흐림", "KPOP", "아이돌", "등교", "일렉트로닉", "청량한", "달달한", "운동", "헬스", "오후", "올디스", "페스티벌", "공부", "감각적인", "사무실", "아침", "쌀쌀한", "지치고", "힘들때", "우울할때", "출근", "방안에서", "2010년대", "새벽감성", "편집숍", "매장", "클럽", "파티", "독서", "어쿠스틱한", "선선한", "목소리", "음색", "OST", "술집", "펍", "폭염", "더위", "애절한", "뉴에이지", "일렉트로닉팝", "크리스마스", "클래식", "공연", "라이브", "잠들기전", "몽환적인", "쓸쓸한", "설렘", "심쿵", "미세먼지", "황사", "혼술", "혼밥", "맑음", "강한", "환절기", "소울풀한", "싱숭생숭", "시원한", "외로울때", "울고", "눈", "상쾌한", "EDM", "호텔", "바", "고백", "멘붕", "불안", "썸", "듀엣", "피처링", "섹시한", "1990년대", "트로트", "노래방", "나들이", "소풍", "걸그룹", "2000년대", "2020년대", "리메이크", "하교", "퇴근", "JPOP", "1980년대", "월드뮤직", "1970년대", "보사노바", "뉴에이지", "BGM", "동요", "키즈", "청춘", "결혼", "CCM", "태교", "릴스", "1960년대", "클래식", "답답할때", "10대", "20대", "패션쇼", "뮤지컬", "국악", "합창", "광고"
+        ]
 
-        # 모든 'Title'을 문자열로 변환
-        mml_music_tag_df['title'] = mml_music_tag_df['title'].astype(str)
+        # 각 형태소에 대해 유사한 태그 찾기 및 유사도 순으로 정렬
+        similar_tags_for_morphs = {}
+        for morph in morphs:
+            similar_tags = find_similar_tags(morph, tags)
+            # 유사도 점수에 따라 태그 정렬 (내림차순)
+            similar_tags.sort(key=lambda x: x[1], reverse=True)
+            if similar_tags:
+                similar_tags_for_morphs[morph] = similar_tags[0][0]
 
-        # 'Tag' 데이터를 텍스트 유사성 분석을 위해 처리합니다.
-        # 먼저, 빈 태그를 제거하고 태그를 일관성을 유지하기 위해 소문자로 변환합니다.
-        tag_data_expanded = tag_data_expanded[tag_data_expanded['tag'].str.strip().ne('')]
-        tag_data_expanded['tag'] = tag_data_expanded['tag'].str.lower()
-
-        # TF-IDF를 위한 고유 태그 목록을 생성합니다.
-        unique_tags = tag_data_expanded['tag'].unique()
-
-        # TF-IDF 벡터 변환기를 초기화합니다.
-        tfidf_vectorizer = TfidfVectorizer()
-
-        # 벡터 변환기를 고유 태그에 맞게 피팅합니다.
-        tfidf_matrix = tfidf_vectorizer.fit_transform(unique_tags)
-
-        # 입력 문장을 여러 키워드를 포함하도록 업데이트합니다.
-        input_keywords = filtered_tokens
-        
-        # 각 키워드를 개별적으로 벡터화합니다.
-        input_vectors = [tfidf_vectorizer.transform([keyword]) for keyword in input_keywords]
-
-        # 각 키워드와 모든 태그 간의 코사인 유사성을 계산합니다.
-        cosine_similarities_keywords = [cosine_similarity(input_vector, tfidf_matrix).flatten() for input_vector in input_vectors]
-        print(cosine_similarities_keywords)
-        # 각 키워드에 대해 가장 유사한 태그를 찾습니다.
-        most_similar_tags = [unique_tags[np.argmax(cosine_similarities)] for cosine_similarities in cosine_similarities_keywords]
-        similarity_scores = [np.max(cosine_similarities) for cosine_similarities in cosine_similarities_keywords]
-        print(most_similar_tags)
-        print(similarity_scores)
-        
-        # 각 키워드에 대한 결과를 표시하기 위한 딕셔너리를 생성합니다.
-        similarity_results = dict(zip(input_keywords, zip(most_similar_tags, similarity_scores)))
-
-        # 이전 단계에서 계산한 가장 유사한 태그 사용
-        # 예를 들어, 'similarity_results' 딕셔너리에서 태그 추출
-        most_similar_tags = [similarity_results[keyword][0] for keyword in input_keywords]
-        print(most_similar_tags)
-        
-        # 해당 태그를 포함하는 음악 리스트 추출
-        music_list = []
-        for tag in most_similar_tags:
-            # 해당 태그를 포함하는 모든 음악 찾기
-            music_with_tag = mml_music_tag_df[mml_music_tag_df['tag'].str.contains(tag)]
-            music_list.append(music_with_tag)
-
-        # 모든 결과를 하나의 DataFrame으로 결합
-        similar_mml_music_tag_df = pd.concat(music_list).drop_duplicates().reset_index(drop=True)
+        # 모든 태그를 포함하는 음악만 필터링
+        similar_mml_music_tag_df = mml_music_tag_df
+        for tag in similar_tags_for_morphs.values():
+            similar_mml_music_tag_df = similar_mml_music_tag_df[similar_mml_music_tag_df['tag'].str.contains(tag)]
 
         genre_reference_df = mml_music_info_df[['title', 'artist', 'genre']].copy()
 
@@ -184,17 +161,6 @@ class tag_song2vec_view(APIView):
         user_profile_vector = create_weighted_lyrics_profile(user_lyrics, w2v_model, weights_dict)
         # user_profile_vector = create_lyrics_profile(user_lyrics, w2v_model)
 
-        # 특정 사용자 ID에 대한 사용자의 청취 기록을 필터링
-        user_specific_log = music_data[music_data['user'] == user_id]
-
-        # 특정 사용자의 장르별 플레이 횟수를 계산
-        user_specific_genre_counts = user_specific_log['genre_user_log'].value_counts()
-
-        # 특정 사용자의 상위 3개 장르를 가져옵니다.
-        user_specific_top_genres = user_specific_genre_counts.head(5).index.tolist()
-
-        # 사용자 상위 장르와 일치하는 노래에 대해 music_total_with_genre 데이터 프레임 필터링
-        user_specific_top_genres_songs_df = music_tag_data[music_tag_data['genre'].isin(user_specific_top_genres)]
 
         # 태그 데이터를 전처리하는 함수를 정의합니다.
         def preprocess_tags(tag_string):
@@ -205,7 +171,7 @@ class tag_song2vec_view(APIView):
             return tags
 
         # 태그 데이터에 전처리 함수를 적용합니다.
-        user_specific_top_genres_songs_df['processed_tags'] = user_specific_top_genres_songs_df['tag'].apply(preprocess_tags)
+        music_tag_data['processed_tags'] = music_tag_data['tag'].apply(preprocess_tags)
 
         # 태그를 벡터로 변환하는 함수를 정의합니다.
         def vectorize_tags(tags, w2v_model):
@@ -218,7 +184,7 @@ class tag_song2vec_view(APIView):
             return np.mean(tag_vectors, axis=0) if tag_vectors else np.zeros(w2v_model.vector_size)
 
         # 각 태그를 벡터로 변환합니다.
-        user_specific_top_genres_songs_df['tag_vector'] = user_specific_top_genres_songs_df['processed_tags'].apply(lambda tags: vectorize_tags(tags, w2v_model))
+        music_tag_data['tag_vector'] = music_tag_data['processed_tags'].apply(lambda tags: vectorize_tags(tags, w2v_model))
 
         # 사용자 프로필 벡터와 모든 태그 벡터 사이의 코사인 유사도를 계산하고 상위 N개의 추천과 함께 유사도를 반환하는 함수
         def recommend_songs_with_similarity(user_profile_vector, tag_vectors, songs_data, top_n=20):
@@ -237,11 +203,11 @@ class tag_song2vec_view(APIView):
             return recommendations_with_scores[['title', 'artist', 'tag', 'similarity']]
 
         # 모든 태그 벡터를 하나의 배열로 추출합니다.
-        tag_vectors_matrix = np.array(list(user_specific_top_genres_songs_df['tag_vector']))
+        tag_vectors_matrix = np.array(list(music_tag_data['tag_vector']))
 
         # 사용자 ID에 대한 노래 추천을 받고 유사도 점수를 포함하여 출력합니다.
         # user_profile_vector_for_similarity = user_profiles[user_id_to_recommend]  # 해당 사용자의 프로필 벡터를 가져옵니다.
-        recommendations_with_similarity = recommend_songs_with_similarity(user_profile_vector, tag_vectors_matrix, user_specific_top_genres_songs_df)
+        recommendations_with_similarity = recommend_songs_with_similarity(user_profile_vector, tag_vectors_matrix, music_tag_data)
         recommendations_with_similarity
 
         # Merge the dataframes on 'Title' and 'Artist' to find matching songs
