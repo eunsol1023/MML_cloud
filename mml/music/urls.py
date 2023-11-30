ad3639b9d5f854e6e9c63794af2b0affa22417bf
pepperonipizza97 <potatopizza715@gmail.com>
2023-11-30 09:33:22 +0900

    Update urls.py

--- mml/music/urls.py
+++ mml/music/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import user_like_artist_view, song2vec_view, tag_song2vec_view, song_info
+from .views import user_like_artist_view, song2vec_view, tag_song2vec_view, song_info, tag_song2vec_his
 
 urlpatterns = [
     path('user_like_artist/', user_like_artist_view.as_view()),
     path('song2vec/', song2vec_view.as_view()),
     path('tag_song2vec/', tag_song2vec_view.as_view()),
     path('song_info/', song_info),
+    path('tag_song2vec_his/', tag_song2vec_his)
 ]
